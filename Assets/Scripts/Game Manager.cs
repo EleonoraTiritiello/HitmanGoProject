0e5b46af8675fb478a14f6453f6b62a86bca5be7
EleonoraTiritiello <ele99.249@gmail.com>
2020-10-12 12:24:31 +0200

    Script Game Manager
    Inseriti: Condizioni di Vittoria e Sconfitta

---
+++ Assets/Scripts/Game Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    #region VARIABLES
+
+    #endregion
+
+    public void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        VictoryCondition();
+        LoseCondition();
+    }
+
+    public void VictoryCondition()
+    {
+        
+    }
+
+    public void LoseCondition()
+    {
+        
+    }
+}
