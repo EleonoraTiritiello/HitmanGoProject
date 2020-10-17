31e7fd2e13cfbe55b8eafa85a13d5125d413c0eb
EleonoraTiritiello <ele99.249@gmail.com>
2020-10-17 11:23:35 +0200

    Fix Game Manager

---
+++ Assets/GameManager/Scripts/Game Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET.Game_Manager
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        #region VARIABLES
+
+        #endregion
+
+        public void Awake()
+        {
+        
+        }
+
+        private void Update()
+        {
+            VictoryCondition();
+            LoseCondition();
+        }
+
+        public void VictoryCondition()
+        {
+        
+        }
+
+        public void LoseCondition()
+        {
+        
+        }
+    }
+}
