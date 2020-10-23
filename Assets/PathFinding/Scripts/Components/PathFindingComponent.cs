be4b0a34b6b8a25232a71675bdcbab0a8d45d76b
Simone Boerci <boercisimone@gmail.com>
2020-10-23 20:22:11 +0200

    Basic path finding implementation

---
+++ Assets/PathFinding/Scripts/Components/PathFindingComponent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SB.HitmanGO
+{
+    public class PathFindingComponent : MonoBehaviour
+    {
+        #region Public Variables
+
+        public float threshold = 3;
+
+        #endregion
+
+        #region Public Methods
+
+        public Vector3 GetNearestNodePosition(Vector3 origin, Vector3 direction)
+        {
+            Vector3 nearestNodePosition = origin;
+
+            List<Node> nodesInDirection = new List<Node>();
+
+            foreach(Node node in PathFindingManager.GetInstance.GetNodes())
+            {
+                if(direction == Vector3.forward)
+                    if (node.Position.z > origin.z && node.Position.x == origin.x) nodesInDirection.Add(node);
+                if(direction == Vector3.back)
+                    if (node.Position.z < origin.z && node.Position.x == origin.x) nodesInDirection.Add(node);
+                if(direction == Vector3.left)
+                    if (node.Position.x < origin.x && node.Position.z == origin.z) nodesInDirection.Add(node);
+                if (direction == Vector3.right)
+                    if (node.Position.x > origin.x && node.Position.z == origin.z) nodesInDirection.Add(node);
+            }
+
+            if(nodesInDirection.Count > 0)
+            {
+                Node nearestNode = nodesInDirection[0];
+
+                foreach(Node node in nodesInDirection)
+                {
+                    if (Vector3.Distance(origin, nearestNode.Position) > Vector3.Distance(origin, node.Position))
+                        nearestNode = node;
+                }
+
+                nearestNodePosition = nearestNode.Position;
+                nearestNodePosition.y = origin.y;
+
+                if (Vector3.Distance(origin, nearestNode.Position) < threshold)
+                    return nearestNodePosition;
+                else
+                    return origin;
+            }
+
+            return nearestNodePosition;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/PathFinding/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SB.HitmanGO
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region Variables
+
+        #region Public Variables
+
+        [Header("Inputs")]
+        public KeyCode MoveUpKey;
+        public KeyCode MoveDownKey;
+        public KeyCode MoveLeftKey;
+        public KeyCode MoveRightKey;
+
+        [Header("Stats")]
+        public float MovementSpeed = 1f;
+
+        #endregion
+
+        #region Private Variables
+
+        private PathFindingComponent _pfc;
+
+        private Vector3 _targetPosition;
+        private bool _isMoving;
+
+        #endregion
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Awake()
+        {
+            if(_pfc == null) _pfc = GetComponent<PathFindingComponent>();
+
+            _targetPosition = transform.position;
+            _isMoving = false;
+        }
+
+        private void Update()
+        {
+            CheckInputs();
+            Debug.Log(_targetPosition);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CheckInputs()
+        {
+            if (Input.GetKeyDown(MoveUpKey) && !_isMoving)
+            {
+                TargetTopNode();
+                StopAllCoroutines();
+                StartCoroutine("MoveTowardsTargetPosition");
+            }
+            else if (Input.GetKeyDown(MoveDownKey) && !_isMoving)
+            {
+                TargetBottomNode();
+                StopAllCoroutines();
+                StartCoroutine("MoveTowardsTargetPosition");
+            }
+            else if (Input.GetKeyDown(MoveLeftKey) && !_isMoving)
+            {
+                TargetLeftNode();
+                StopAllCoroutines();
+                StartCoroutine("MoveTowardsTargetPosition");
+            }
+            else if (Input.GetKeyDown(MoveRightKey) && !_isMoving)
+            {
+                TargetRightNode();
+                StopAllCoroutines();
+                StartCoroutine("MoveTowardsTargetPosition");
+            }
+        }
+
+        private IEnumerator MoveTowardsTargetPosition()
+        {
+            _isMoving = true;
+
+            while (Mathf.Abs(Vector3.Distance(transform.position, _targetPosition)) >= 1.5f) {
+                transform.Translate((_targetPosition - transform.position) * MovementSpeed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+
+            transform.position = _targetPosition;
+            _isMoving = false;
+        }
+
+        #region Target Node
+
+        private void TargetTopNode()
+        {
+            _targetPosition = _pfc.GetNearestNodePosition(transform.localPosition, Vector3.forward);
+            Debug.LogWarning("Top Node");
+        }
+
+        private void TargetBottomNode()
+        {
+            _targetPosition = _pfc.GetNearestNodePosition(transform.localPosition, Vector3.back);
+            Debug.LogWarning("Bottom Node");
+        }
+
+        private void TargetLeftNode()
+        {
+            _targetPosition = _pfc.GetNearestNodePosition(transform.localPosition, Vector3.left);
+            Debug.LogWarning("Left Node");
+        }
+
+        private void TargetRightNode()
+        {
+            _targetPosition = _pfc.GetNearestNodePosition(transform.localPosition, Vector3.right);
+            Debug.LogWarning("Right Node");
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
