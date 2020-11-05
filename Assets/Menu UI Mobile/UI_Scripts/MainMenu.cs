9e45ea2a4b79cecde1925e090fe95f811d5100ee
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-11-05 21:10:11 +0100

    MainMenu script done

--- Assets/Menu UI Mobile/UI_Scripts/MainMenu.cs
+++ Assets/Menu UI Mobile/UI_Scripts/MainMenu.cs
@@ -1,18 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject mainMenuTopLeftZone;
+    [SerializeField]
+    private GameObject mainMenuTopRightZone;
+    [SerializeField]
+    private GameObject mainMenuMiddleCenterZone;
+    [SerializeField]
+    private GameObject optionsMenu;
+    [SerializeField]
+    private GameObject boxSelectionTopLeftZone;
+    [SerializeField]
+    private GameObject boxSelectionTopRightZone;
+    [SerializeField]
+    private GameObject boxSelectionBottomMiddleZone;
+    [SerializeField]
+    private GameObject blackPanelObject;
+    [SerializeField]
+    private Image blackPanelImage;
+
+    public void GoButtonPressed()
     {
-        
+        //Sequenza quando viene premuto il pulsante GO(forse era meglio una coroutine ma sono designer)
+        BlackPanelAppears();
+        FadeIn();
+        Invoke("MainMenuDisappears", 0.4f);
+        Invoke("BoxSelectionAppears", 0.4f);
+        Invoke("FadeOut", 0.5f);
+        Invoke("BlackPanelDisappears", 1f);
+        Debug.Log("GoButtonClicked");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OptionButtonPressed()
     {
-        
+        //Sequenza quando viene premuto il pulsante opzioni(forse era meglio una coroutine ma sono designer)
+        BlackPanelAppears();
+        FadeIn();
+        Invoke("MainMenuDisappears", 0.4f);
+        Invoke("OptionsMenuAppears", 0.4f);
+        Invoke("FadeOut", 0.5f);
+        Invoke("BlackPanelDisappears", 1f);
+        Debug.Log("OptionButtonClicked");
+
+    }
+
+    private void MainMenuDisappears()
+    {
+        //disattiva immagini e pulsanti del menu inziale
+        mainMenuTopLeftZone.SetActive(false);
+        mainMenuTopRightZone.SetActive(false);
+        mainMenuMiddleCenterZone.SetActive(false);
+    }
+    private void BoxSelectionAppears()
+    {
+        //attiva immagini e pulsanti della box selection
+        boxSelectionTopLeftZone.SetActive(true);
+        boxSelectionTopRightZone.SetActive(true);
+        boxSelectionBottomMiddleZone.SetActive(true);
+    }
+    private void OptionsMenuAppears()
+    {
+        optionsMenu.SetActive(true);
+    }
+    private void FadeIn()
+    {
+        //cambia l'alpha del pannello nero a 1(totalmente nero) in X secondi(secondo paramentro) dopo averla impostata a 0
+        blackPanelImage.canvasRenderer.SetAlpha(0f);
+        blackPanelImage.CrossFadeAlpha(1, 0.4f, false);
+    }
+    private void FadeOut()
+    {
+        //cambia l'alpha del pannello nero a 0(totalmente trasparente) in X secondi(secondo paramentro)
+        blackPanelImage.CrossFadeAlpha(0, 0.5f, false);
+    }
+    private void BlackPanelAppears()
+    {
+        blackPanelObject.SetActive(true);
+    }
+    private void BlackPanelDisappears()
+    {
+        blackPanelObject.SetActive(false);
     }
 }
