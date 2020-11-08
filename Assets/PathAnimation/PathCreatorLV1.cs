2e1ab2b7c95cf5741b8faf18167ae3f41ad9beaf
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-11-08 20:23:27 +0100

    Lv1 creator done

--- Assets/PathAnimation/PathCreatorLV1.cs
+++ Assets/PathAnimation/PathCreatorLV1.cs
@@ -31,74 +31,84 @@ public class PathCreatorLV1 : MonoBehaviour
 
     public void Update()
     {
-        for (phaseNumber = 0; phaseNumber < 5;)
+        if (phaseNumber == 0)
         {
-            if (phaseNumber == 0)
-            {
-                Node1fill();
-            }
+            Node1fill();
+        }
 
-            if (phaseNumber == 1)
-            {
-                Segment1fill();
-            }
+        if (phaseNumber == 1)
+        {
+            Segment1fill();
+        }
 
-            if (phaseNumber == 2)
-            {
-                Node2fill();
-            }
+        if (phaseNumber == 2)
+        {
+            Node2fill();
+        }
 
-            if (phaseNumber == 3)
-            {
-                Segment2fill();
-            }
+        if (phaseNumber == 3)
+        {
+            Segment2fill();
+        }
 
-            if (phaseNumber == 4)
-            {
-                Node3fill();
-            }
+        if (phaseNumber == 4)
+        {
+            Node3fill();
+            Debug.Log("FINITO");
+        }
+        if (phaseNumber == 5)
+        {
+
         }
     }
 
     private void Node1fill()
     {
-        node1.fillAmount += 1.0f * nodeFillTimeCoefficient * Time.deltaTime;
+        node1.fillAmount += 1.0f / nodeFillTimeCoefficient * Time.deltaTime;
         if (node1.fillAmount == 1)
         {
             phaseNumber++;
+            Debug.Log("+1");
         }
     }
     private void Node2fill()
     {
-        node2.fillAmount += 1.0f * nodeFillTimeCoefficient * Time.deltaTime;
+        node2.fillAmount += 1.0f / nodeFillTimeCoefficient * Time.deltaTime;
         if (node2.fillAmount == 1)
         {
             phaseNumber++;
+            Debug.Log("+1");
+
         }
     }
 
     private void Node3fill()
     {
-        node3.fillAmount += 1.0f * nodeFillTimeCoefficient * Time.deltaTime;
+        node3.fillAmount += 1.0f / nodeFillTimeCoefficient * Time.deltaTime;
         if (node3.fillAmount == 1)
         {
             phaseNumber++;
+            Debug.Log("+1");
+
         }
     }
     private void Segment1fill()
     {
-        segment1.fillAmount += 1.0f * segmentFillTimeCoefficient * Time.deltaTime;
+        segment1.fillAmount += 1.0f / segmentFillTimeCoefficient * Time.deltaTime; ;
         if (segment1.fillAmount == 1)
         {
             phaseNumber++;
+            Debug.Log("+1");
+
         }
     }
     private void Segment2fill()
     {
-        segment2.fillAmount += 1.0f * segmentFillTimeCoefficient * Time.deltaTime;
+        segment2.fillAmount += 1.0f / segmentFillTimeCoefficient * Time.deltaTime;
         if (segment2.fillAmount == 1)
         {
             phaseNumber++;
+            Debug.Log("+1");
         }
     }
 }
