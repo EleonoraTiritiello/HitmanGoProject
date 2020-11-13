92b739f79412b0ffb9bec4fd7a163ae33ae27070
tommi <tommiconlai@gmail.com>
2020-11-13 10:37:42 +0100

    ora c'è lo script lol

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject arrive;
+    public GameObject Cam;
+    public float speed;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        Cam.transform.position = Vector3.Lerp(Cam.transform.position, arrive.transform.position, speed * Time.deltaTime);
+        //Cam.transform.position = arrive.transform.position;
+    }
+
+}
