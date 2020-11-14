093bfcfa9077ff6b4b680711f586294954f6fc90
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-11-14 18:30:39 +0100

    Enemies prefabbed with checker

---
+++ Assets/Rock/enemyReactions/RockCheck.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RockCheck : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("checker"))
+        {
+            Debug.Log("ALLARMATO");
+            this.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        }
+    }
+}
