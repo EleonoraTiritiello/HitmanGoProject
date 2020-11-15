b4fcbb3805be67c042f7cbcfa56a3955081951ce
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-11-16 00:24:50 +0100

    Part4

---
+++ Assets/EnemyProvisory/EnemyKillPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyKillPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(player);
+            Invoke("ReloadScene", 3f);
+        }
+
+
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/EnemyProvisory/EnemyScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(enemy);
+        }
+    }
+}
