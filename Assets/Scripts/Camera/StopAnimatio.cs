7a5e5489877660edc2414caff1156bc9bbc17da6
tommi <tommiconlai@gmail.com>
2020-11-16 17:39:57 +0100

    start level animation script

---
+++ Assets/Scripts/Camera/StopAnimatio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAnimatio : MonoBehaviour
+{
+    public GameObject myAnimation;
+    private void faiquellochetidico()
+    {
+        myAnimation.gameObject.GetComponent<Animator>().enabled = false;
+    }
+}
