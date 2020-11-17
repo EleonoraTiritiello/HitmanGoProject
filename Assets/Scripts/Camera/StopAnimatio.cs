8311ce83d33487be8afab33827db2265ff977a57
tommi <tommiconlai@gmail.com>
2020-11-17 09:54:52 +0100

    Start animation change

--- Assets/Scripts/Camera/StopAnimatio.cs
+++ Assets/Scripts/Camera/StopAnimatio.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class StopAnimatio : MonoBehaviour
 {
     public GameObject myAnimation;
+    public GameObject animationON;
     private void faiquellochetidico()
     {
         myAnimation.gameObject.GetComponent<Animator>().enabled = false;
+        animationON.SetActive(true);
     }
 }
