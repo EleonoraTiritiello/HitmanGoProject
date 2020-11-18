46ec8cf783ef1ebbe600678b313399d0ac6bb62f
Chiara Sanviti <chiara637288@gmail.com>
2020-11-18 12:34:34 +0100

    New Sfx and 16 sizes

--- Assets/Sounds/Script/SoundMethos.cs
+++ Assets/Sounds/Script/SoundMethos.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class SoundMethos : MonoBehaviour
 {
-
+    //questo è il suono che si sente quando il giocatore arriva alla fine del livello e compare 1 sola carta ( quindi si usa per tutti i livelli dall 1-1 all 1-5) 
     public void LevelCompletePlay() 
     {
         AudioManager.Instance.PlaySound("LevelComplete");
     }
-
-    /*public void LevelCompletePlay()
+    // questo è quando i nodi (percorso che deve fare agente47) vengono creati ad inizio livello 
+    public void CreationNodesPlay()
     {
-        AudioManager.Instance.PlaySound("LevelComplete");
+        AudioManager.Instance.PlaySound("CreationNodes");
     }
-    */
-    
+
+    //
+    public void CreationNodesPlay()
+    {
+        AudioManager.Instance.PlaySound("CreationNodes");
+    }
+
+
+
     // StopSound va messo solo per le musiche che quindi vanno in loop (questo sotto andrà eliminato) 
     public void LevelCompleteStop()
     {
