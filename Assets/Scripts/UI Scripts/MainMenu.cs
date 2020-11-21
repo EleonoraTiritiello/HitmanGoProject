4fc891b7860f269838eae6a2992f6e284e05a363
EleonoraTiritiello <ele99.249@gmail.com>
2020-11-21 14:50:52 +0100

    Pulizia Merge

--- Assets/Scripts/UI Scripts/MainMenu.cs
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -19,56 +19,6 @@ namespace HitmanGO
 
         #endregion
 
-        public void GoButtonPressed()
-        {
-            //Sequenza quando viene premuto il pulsante GO(forse era meglio una coroutine ma sono designer)
-            StartCoroutine(UIMenu.GetInstance.FadeIn());
-            StartCoroutine(UIMenu.GetInstance.BlackPanelAppears());
-            StartCoroutine(UIMenu.GetInstance.FadeOut());
-            StartCoroutine(UIMenu.GetInstance.BlackPanelDisappears());
-
-            Debug.Log("GoButtonClicked");
-        }
-
-        public void OptionButtonPressed()
-        {
-            //Sequenza quando viene premuto il pulsante opzioni(forse era meglio una coroutine ma sono designer)
-            StartCoroutine(UIMenu.GetInstance.FadeIn());
-            StartCoroutine(UIMenu.GetInstance.BlackPanelAppears());
-            StartCoroutine(UIMenu.GetInstance.FadeOut());
-            StartCoroutine(UIMenu.GetInstance.BlackPanelDisappears());
-
-            Debug.Log("OptionButtonClicked");
-        }
-
-        private void FadeIn()
-        {
-
-            //cambia l'alpha del pannello nero a 1(totalmente nero) in X secondi(secondo paramentro) dopo averla impostata a 0
-            _blackPanel.canvasRenderer.SetAlpha(0f);
-            _blackPanel.CrossFadeAlpha(1, 0.4f, true);
-        }
-
-        private void FadeOut()
-        {
-
-            //cambia l'alpha del pannello nero a 0(totalmente trasparente) in X secondi(secondo paramentro)
-            _blackPanel.CrossFadeAlpha(0, 0.4f, false);
-        }
-
-        private void BlackPanelAppears()
-        {
-
-            //disattiva il gameobject del pannello nero
-
-            _blackObject.SetActive(true);
-        }
-
-        private void BlackPanelDisappears()
-        {
-            //attiva il gameobject del pannello nero
-            _blackObject.SetActive(false);
-        }
 
         public void OnOptionsButtonClicked()
         {
