24554fb166de1500cf4206181ba88f23012cb37f
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-11-22 22:53:56 +0100

    Cameras

---
+++ Assets/CameraStuff/AnimationCamera3.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCamera3 : MonoBehaviour
+{
+    public GameObject myAnimation;
+    public GameObject animationON;
+    public GameObject animLookAt;
+
+    private void StartLevel()
+    {
+        myAnimation.gameObject.GetComponent<Animator>().enabled = false;
+        animationON.SetActive(true);
+        animLookAt.SetActive(false);
+    }
+}
---
+++ Assets/CameraStuff/StopAnimatio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAnimatio : MonoBehaviour
+{
+    public GameObject myAnimation;
+    public GameObject animationON;
+    private void animationQuit()
+    {
+        myAnimation.gameObject.GetComponent<Animator>().enabled = false;
+        animationON.SetActive(true);
+    }
+}
