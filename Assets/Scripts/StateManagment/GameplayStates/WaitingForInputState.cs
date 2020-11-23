07832843d665d0294282bd7cca835877d49231c2
Nicolas <betteganicolas@hotmail.com>
2020-11-23 17:39:09 +0100

    Added Go pulsanting animation & swipe

--- Assets/Scripts/StateManagment/GameplayStates/WaitingForInputState.cs
+++ Assets/Scripts/StateManagment/GameplayStates/WaitingForInputState.cs
@@ -6,6 +6,7 @@ namespace HitmanGO
     {
         private InputManager _inputManager;
         private PlayerController _player;
+        private Swipe _swipeControl;
 
 
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -24,27 +25,48 @@ namespace HitmanGO
                         SetPlayerAction(PlayerController.Actions.PickupRock);
                 }
             }
+            if (_player == null) _player = GameManager.GetInstance.Player;
+            if (_swipeControl == null) _swipeControl = _player.GetComponent<Swipe>();
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             if (_player.CurrentState == PlayerController.States.Idle)
             {
-                if (Input.GetKeyDown(_inputManager.SelectPlayerKey)) {
+                if (Input.GetKeyDown(_inputManager.SelectPlayerKey) || _swipeControl.tap == true)
+                {
                     if (_player.ToDoAction != PlayerController.Actions.PickupRock)
+                    {
                         SetPlayerAction(PlayerController.Actions.Select);
+                    }
+
                 }
+
+                if (Input.GetKeyDown(_inputManager.DieKey))
+                    SetPlayerAction(PlayerController.Actions.Die);
             }
             else if (_player.CurrentState == PlayerController.States.Selected)
             {
-                if (Input.GetKeyDown(_inputManager.MoveUpKey))
+                if (Input.GetKeyDown(_inputManager.MoveUpKey) || _swipeControl.swipeUp)
+                {
                     SetPlayerAction(PlayerController.Actions.MoveUp);
-                else if (Input.GetKeyDown(_inputManager.MoveDownKey))
+                    _swipeControl.playerSelect = false;
+                }
+                else if (Input.GetKeyDown(_inputManager.MoveDownKey) || _swipeControl.swipeDown)
+                {
                     SetPlayerAction(PlayerController.Actions.MoveDown);
-                else if (Input.GetKeyDown(_inputManager.MoveLeftKey))
+                    _swipeControl.playerSelect = false;
+                }
+                else if (Input.GetKeyDown(_inputManager.MoveLeftKey) || _swipeControl.swipeLeft)
+                {
                     SetPlayerAction(PlayerController.Actions.MoveLeft);
-                else if (Input.GetKeyDown(_inputManager.MoveRightKey))
+                    _swipeControl.playerSelect = false;
+                }
+                else if (Input.GetKeyDown(_inputManager.MoveRightKey) || _swipeControl.swipeRight)
+                {
                     SetPlayerAction(PlayerController.Actions.MoveRight);
+                    _swipeControl.playerSelect = false;
+                }
             }
         }
 
